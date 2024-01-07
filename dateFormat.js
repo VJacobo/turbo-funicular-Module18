bf93932c5b12179671962200d028b88e02e113b2
VJacobo <victoriajacobo99@gmail.com>
2024-01-06 22:00:45 -0600

    added date and time calculations to dateFormate.js

---
+++ dateFormat.js
@@ -0,0 +1,10 @@
+module.exports = (timestamp) => {
+    const date = new Date(timestamp);
+    const year = date.getFullYear();
+    const month = '${date.getMonth() + 1}'.padStart(2, '0');
+    const day = '${date.getDate()}'.padStart(2, '0');
+    const hours = '${date.getHours()}'.padStart(2, '0');
+    const minutes = '${date.getMinutes()}'.padStart(2, '0');
+    const seconds = '${date.getSeconds()}'.padStart(2, '0');
+    return '${year}-${month}-${hours}:${minutes}:${seconds}';
+};
